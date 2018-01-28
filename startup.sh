5b88823b14fbba7f0bdea337041c99f50265abb6
bednar <bednar@apitea.com>
2018-01-28 17:17:38 +0100

    * startup script generate log files

--- startup.sh
+++ startup.sh
@@ -13,6 +13,17 @@ echo "*               docker-spring-micro-services               *"
 echo "************************************************************"
 echo
 
+STARTUP_OPTIONS_PATH=$(cd `dirname $0` && pwd)/startup.options
+
+#
+# Config
+#
+echo "Configuration"
+echo "-------------"
+echo
+echo " * startup.options path: " $STARTUP_OPTIONS_PATH
+echo
+
 
 #
 # Print recipes
@@ -21,7 +32,7 @@ echo "Recipes used in the Container"
 echo "-----------------------------"
 echo
 
-cat startup.options | \
+cat $STARTUP_OPTIONS_PATH | \
     jq -rc '.recipes[]' | while IFS='' read stack;do
         recipeName=$(echo "$stack" | jq -r .name)
         recipeVersion=$(echo "$stack" | jq -r .version)
@@ -36,12 +47,18 @@ echo "Create Log Files"
 echo "----------------"
 echo
 
-cat startup.options | \
+cat $STARTUP_OPTIONS_PATH | \
     jq -rc '.recipes[]' | while IFS='' read stack;do
         recipeName=$(echo "$stack" | jq -rc .name)
         recipeLogs=$(echo "$stack" | jq -rc '.logFiles[]')
 
         if [ ! -z "$recipeLogs" ]; then
             echo " *" $recipeName":" $recipeLogs
+            for recipeLog in $recipeLogs
+            do
+                mkdir -p `dirname $recipeLog`;
+                touch $recipeLog;
+                chmod a+w $recipeLog;
+            done
         fi
     done
