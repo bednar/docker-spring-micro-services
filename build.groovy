628390042d01d9a37f395dab1c25684cc966cd64
bednar <bednar@apitea.com>
2018-02-02 15:29:34 +0100

    * The Startup Script improvements: Start Services, Provisioning, Multitail  * Initial version of Couchbase

--- build.groovy
+++ build.groovy
@@ -1,21 +1,18 @@
 import groovy.text.SimpleTemplateEngine
 
 class StartupOptions { List<StartupOptionsRecipe> recipes = []}
-class StartupOptionsRecipe { String name; String version; String service; List<String> logFiles = []; List<String> provisioningScripts = [] }
+class StartupOptionsRecipe { String name; String version; String service = ""; List<String> logFiles = []; List<String> provisioningScripts = [] }
 
 println '[1/3] Processing Recipes... '
 
 def startupOptions = new StartupOptions()
 def templateText = new File('./Dockerfile.tmpl').text
 def recipesText = ""
-def logFiles = []
 def exposePorts = []
-def services = []
-def provisioningScripts = ['echo Start provisioning']
 
 // parse recipes
 def jsonSlurper = new  groovy.json.JsonSlurper()
-['java', 'maven', 'gradle', 'elasticsearch', 'kibana', 'logstash', 'zookeeper', 'kafka'].each {
+['java', 'maven', 'gradle', 'elasticsearch', 'kibana', 'logstash', 'zookeeper', 'kafka', 'couchbase'].each {
 
 	def path = './recipes/' + it +'.json'
 	def recipe = jsonSlurper.parseText(new File(path).text)
@@ -35,21 +32,18 @@ def jsonSlurper = new  groovy.json.JsonSlurper()
 
 	if (recipe.provisioningScripts)
 	{
-		provisioningScripts = provisioningScripts.plus(recipe.provisioningScripts)
 		startupOptionsRecipe.provisioningScripts.addAll(recipe.provisioningScripts)
 	}
 
 	// Log files
 	if (recipe.logFiles)
 	{
-		logFiles = logFiles.plus(recipe.logFiles)
 		startupOptionsRecipe.logFiles.addAll(recipe.logFiles)
 	}
 
 	// Service
 	if (recipe.service)
 	{
-		services << recipe.service
 		startupOptionsRecipe.service = recipe.service
 	}
 
@@ -67,25 +61,9 @@ def jsonSlurper = new  groovy.json.JsonSlurper()
 
 println()
 
-def multiTailColors = ["green", "yellow", "blue", "magenta", "cyan", "white", "red"] as LinkedList
-def multiTailArgs = logFiles.collect
-{
-	// get first and remove
-	def color = multiTailColors.poll();
-	// add to end
-	multiTailColors.add(color)
-
-	"-ci $color -I $it"
-}.join(' ')
-
 templateText = new SimpleTemplateEngine()
 		.createTemplate(templateText)
-		.make(["recipes": recipesText,
-			   "logFiles": logFiles.join(' '),
-			   "multiTailArgs": multiTailArgs,
-			   "exposePorts": exposePorts.join(' '),
-			   "provisioningScripts" : provisioningScripts.join('; '),
-			   "services": services.join(' ')])
+		.make(["recipes": recipesText, "exposePorts": exposePorts.join(' ')])
 
 print '[2/3] Generating the Dockerfile... '
 
--- startup.sh
+++ startup.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 #
 # The Startup script which care about:
 #
@@ -6,6 +6,11 @@
 # * Start services
 # * Run health-check scripts
 # * Run provisioning scripts
+#
+#
+# Thx for the inspiration:
+#
+# * https://superuser.com/questions/186272/check-if-any-of-the-parameters-to-a-bash-script-match-a-string
 #
 
 echo "************************************************************"
@@ -45,9 +50,13 @@ cat $STARTUP_OPTIONS_PATH | \
 echo
 echo "Create Log Files"
 echo "----------------"
-echo
 
-cat $STARTUP_OPTIONS_PATH | \
+if [[ "${@#--createLogFiles}" = "$@" ]]
+then
+    echo "disabled"
+else
+    echo
+    cat $STARTUP_OPTIONS_PATH | \
     jq -rc '.recipes[]' | while IFS='' read stack;do
         recipeName=$(echo "$stack" | jq -rc .name)
         recipeLogs=$(echo "$stack" | jq -rc '.logFiles[]')
@@ -62,3 +71,92 @@ cat $STARTUP_OPTIONS_PATH | \
             done
         fi
     done
+fi
+
+#
+# Start services
+#
+echo
+echo "Start Services"
+echo "--------------"
+
+if [[ "${@#--startServices}" = "$@" ]]
+then
+    echo "disabled"
+else
+    echo
+    cat $STARTUP_OPTIONS_PATH | \
+    jq -rc '.recipes[]' | while IFS='' read stack;do
+        recipeName=$(echo "$stack" | jq -rc .name)
+        recipeService=$(echo "$stack" | jq -rc '.service')
+
+        if [ -n "$recipeService" ]; then
+            echo " *" $recipeName":" $recipeService
+            service $recipeService start;
+        fi
+    done
+fi
+
+#
+# Run Provisioning Scripts
+#
+echo
+echo "Run Provisioning Scripts"
+echo "------------------------"
+
+if [[ "${@#--provisioning}" = "$@" ]]
+then
+    echo "disabled"
+else
+    echo
+    cat $STARTUP_OPTIONS_PATH | \
+    jq -rc '.recipes[]' | while IFS='' read stack;do
+        recipeName=$(echo "$stack" | jq -rc .name)
+        provisioningScripts=$(echo "$stack" | jq -rc '.provisioningScripts[]')
+
+        if [ ! -z "$provisioningScripts" ]; then
+            echo " *" $recipeName":"
+            for provisioningScript in $provisioningScripts
+            do
+                echo "  - run script:" $provisioningScript
+                $provisioningScript
+            done
+        fi
+    done
+fi
+
+#
+# Use Multitail for display log files
+#
+echo
+echo "Use Multitail for display log files"
+echo "-----------------------------------"
+
+if [[ "${@#--multitail}" = "$@" ]]
+then
+    echo "disabled"
+else
+    echo
+    multiTailColors=("green" "yellow" "blue" "magenta" "cyan" "white" "red")
+    multiTailArgs=
+
+    # Every log file has own color
+    for logFile in `cat $STARTUP_OPTIONS_PATH | jq -rc '.recipes[] | .logFiles[]'`
+    do
+        # Pick first color
+        multiTailColor=${multiTailColors[0]}
+        # Shift array
+        multiTailColors=("${multiTailColors[@]:1}")
+        # Add to end of the array
+        multiTailColors=( "${multiTailColors[@]}" "${multiTailColor}" )
+
+        multiTailArg=" -ci $multiTailColor -I $logFile"
+        echo " *" $multiTailArg
+
+        multiTailArgs+=$multiTailArg
+    done
+
+    echo
+    multitail -M 0 --follow-all $multiTailArgs
+fi
+
